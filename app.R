4ef6e261b616f4c7a2eb34cd856b4577d174b0c3
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-19 14:10:16 -0600

    Add more lines

--- app.R
+++ app.R
@@ -27,40 +27,50 @@ server <- function(input, output) {
     day_to_month_day <- function(day) {
         # convert day of year to month and day
         days_in_month <- c(31, 28, 31, 30, 31, 30, 31, 31, 30,
-            31, 30, 31)  #number of days in a month
+            31, 30, 31)
+        # number of days in a month
         month_names <- c("January", "February", "March", "April",
             "May", "June", "July", "August", "September", "October",
-            "November", "December")  #full month names
-        month <- 1  #set initial month to 1 (January)
+            "November", "December")
+        # full month names
+        month <- 1
+        # set initial month to 1 (January)
         while (day > days_in_month[month]) {
             # while day is greater than days in a specified
             # month
-            day <- day - days_in_month[month]  #subtract days by days in a specified month
-            month <- month + 1  #increase month by 1
+            day <- day - days_in_month[month]
+            # subtract days by days in a specified month
+            month <- month + 1
+            # increase month by 1
         }
-        return(list(month = month_names[month], day = day))  #return list of month and day number
+        return(list(month = month_names[month], day = day))
+        # return list of month and day number
     }
 
     sun_longitude <- function(date) {
         # get sun longitude based on day of year (date)
-        return((date - 80) * 360/365)  #in degrees
+        return((date - 80) * 360/365)
+        # in degrees
     }
 
     longitude_to_right_ascension <- function(longitude) {
         # convert ecliptic longitude to right ascension
         return(atan2(sin(longitude * pi/180) * cos(23.45 * pi/180),
-            cos(longitude * pi/180)) * 180/pi)  #in degrees
+            cos(longitude * pi/180)) * 180/pi)
+        # in degrees
     }
 
     sun_declination <- function(date) {
         return(asin(sin(23.44 * pi/180) * sin(sun_longitude(date) *
-            pi/180)) * 180/pi)  #in degrees
+            pi/180)) * 180/pi)
+        # in degrees
     }
 
     sun_rise_set <- function(latitude, declination) {
         # get sunrise and sunset times
         hour_angle <- acos(-tan(latitude * pi/180) * tan(declination *
-            pi/180)) * 180/pi  #in degrees
+            pi/180)) * 180/pi
+        # in degrees
         sunrise <- 12 - hour_angle/15
         sunset <- 12 + hour_angle/15
         return(list(sunrise = sunrise, sunset = sunset))
@@ -69,40 +79,50 @@ server <- function(input, output) {
     planet_longitude <- function(date, elongation) {
         # get planet longitude based on day of year (date)
         # and planet elongation
-        return((date - 80) * 360/365 + elongation)  #in degrees
+        return((date - 80) * 360/365 + elongation)
+        # in degrees
     }
 
     planet_declination <- function(date, elongation) {
         # get planet declination based on planet longitude
         return(asin(sin(23.44 * pi/180) * sin(planet_longitude(date,
-            elongation) * pi/180)) * 180/pi)  #in degrees
+            elongation) * pi/180)) * 180/pi)
+        # in degrees
     }
 
     planet_rise_set <- function(latitude, declination, elongation) {
         # get planet rise and set times based on latitude,
         # declination and planet elongation
-        sun_right_ascension <- longitude_to_right_ascension(sun_longitude(input$date))  #in degrees
+        sun_right_ascension <- longitude_to_right_ascension(sun_longitude(input$date))
+        # in degrees
         planet_right_ascension <- longitude_to_right_ascension(planet_longitude(input$date,
-            elongation))  #in degrees
+            elongation))
+        # in degrees
         hour_angle <- acos(-tan(latitude * pi/180) * tan(declination *
-            pi/180)) * 180/pi  #in degrees
+            pi/180)) * 180/pi
+        # in degrees
         rise <- (12 - hour_angle/15 - (sun_right_ascension -
-            planet_right_ascension)/360 * 24)%%24  #in hours
+            planet_right_ascension)/360 * 24)%%24
+        # in hours
         set <- (12 + hour_angle/15 - (sun_right_ascension - planet_right_ascension)/360 *
-            24)%%24  #in hours
+            24)%%24
+        # in hours
         return(list(rise = rise, set = set))
     }
 
     altitude_azimuth <- function(latitude, declination, time) {
         # get altitude and azimuth based on latitude,
         # declination and time (hour angle)
-        hour_angle <- (time - 12) * 15  #in hours
+        hour_angle <- (time - 12) * 15
+        # in hours
         altitude <- asin(sin(latitude * pi/180) * sin(declination *
             pi/180) + cos(latitude * pi/180) * cos(declination *
-            pi/180) * cos(hour_angle * pi/180)) * 180/pi  #in degrees
+            pi/180) * cos(hour_angle * pi/180)) * 180/pi
+        # in degrees
         azimuth <- acos(min(1, max(-1, (sin(declination * pi/180) -
             sin(altitude * pi/180) * sin(latitude * pi/180))/(cos(altitude *
-            pi/180) * cos(latitude * pi/180))))) * 180/pi  #in degrees
+            pi/180) * cos(latitude * pi/180))))) * 180/pi
+        # in degrees
         if (hour_angle > 0) {
             azimuth <- 360 - azimuth
         }
@@ -111,8 +131,10 @@ server <- function(input, output) {
 
     time_to_hh_mm <- function(time) {
         # convert time to hh:mm.mm format
-        sign <- ifelse(time < 0, "-", "")  #add a minus sign if time is negative
-        time <- abs(time)  #use absolute value of time
+        sign <- ifelse(time < 0, "-", "")
+        # add a minus sign if time is negative
+        time <- abs(time)
+        # use absolute value of time
         hours <- floor(time)
         minutes <- (time - hours) * 60
         return(sprintf("%s%02d:%05.2f", sign, hours, minutes))
@@ -120,8 +142,10 @@ server <- function(input, output) {
 
     output$dateTime <- renderText({
         # date and time output
-        date <- input$date  #date input
-        time <- input$time  #time input
+        date <- input$date
+        # date input
+        time <- input$time
+        # time input
         month_day <- day_to_month_day(date)
         paste("Date: ", month_day$month, " ", month_day$day,
             "\nTime: ", time_to_hh_mm(time))
@@ -130,7 +154,8 @@ server <- function(input, output) {
     output$sunRiseSet <- renderText({
         # sun rise and set output
         date <- input$date
-        latitude <- input$latitude  #latitude input
+        latitude <- input$latitude
+        # latitude input
         declination <- sun_declination(date)
         rise_set <- sun_rise_set(latitude, declination)
         paste("Sunrise: ", time_to_hh_mm(rise_set$sunrise), "\nSunset: ",
@@ -161,7 +186,8 @@ server <- function(input, output) {
     output$planetPosition <- renderText({
         # planet position output
         date <- input$date
-        elongation <- input$elongation  #planet elongation input
+        elongation <- input$elongation
+        # planet elongation input
         longitude <- planet_longitude(date, elongation)
         declination <- planet_declination(date, elongation)
         right_ascension <- longitude_to_right_ascension(longitude)
@@ -185,7 +211,9 @@ server <- function(input, output) {
         # planet altitude and azimuth output
         date <- input$date
         time <- input$time + 24/360 * (longitude_to_right_ascension(planet_longitude(date,
-            input$elongation)) - longitude_to_right_ascension(sun_longitude(date)))  #adjust time based on planet right ascension and sun right ascension
+            input$elongation)) - longitude_to_right_ascension(sun_longitude(date)))
+        # adjust time based on planet right ascension and
+        # sun right ascension
         latitude <- input$latitude
         elongation <- input$elongation
         declination <- planet_declination(date, elongation)
